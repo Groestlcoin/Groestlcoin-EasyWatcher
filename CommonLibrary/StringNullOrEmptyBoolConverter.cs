95df0d3ee533b0b7315f1dc1b7ad5549fb1446d6
Yokomoko <yokomoko_chapster@live.co.uk>
2018-09-10 14:41:10 +0100

    Hide local currency value when currency symbol has not been set

---
+++ CommonLibrary/StringNullOrEmptyBoolConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace MVVMLibrary {
+    public class StringNullOrEmptyBoolConverter : IValueConverter {
+        /// <summary>Returns false if string is null or empty
+        /// 
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
+            var s = value as string;
+            return !string.IsNullOrWhiteSpace(s) ? Visibility.Visible : Visibility.Hidden;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
+            throw new NotImplementedException();
+        }
+    }
+}
