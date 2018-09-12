19174db209e9324e43f628435fde89ed75a422be
Yokomoko <yokomoko_chapster@live.co.uk>
2018-09-12 09:02:25 +0100

    fix groestlhash validation

--- BitcoinLibrary/Base58.cs
+++ BitcoinLibrary/Base58.cs
@@ -1,48 +1,31 @@
 ﻿using System;
-using System.Linq;
-using System.Numerics;
-using System.Security.Cryptography;
-using NBitcoin;
 using NBitcoin.Altcoins;
 
 namespace GroestlcoinLibrary {
     public static class Base58 {
         /// <summary>
-        /// Checks to see if a given string (grs address) is base58 encoded or not.
+        /// Checks to see if a given string (groestlcoin address) is base58 encoded or not.
         /// </summary>
         /// <param name="grsAddress">Groestlcoin address to check</param>
         /// <returns>True if base58 encoded</returns>
         public static VerificationResult Verify(string grsAddress) {
-            VerificationResult result = new VerificationResult() { IsVerified = false };
+            VerificationResult result = new VerificationResult { IsVerified = false };
             if (string.IsNullOrWhiteSpace(grsAddress)) {
                 result.Error = "Address can not be empty!";
                 return result;
             }
             if (!grsAddress.StartsWith("F") && !grsAddress.StartsWith("3")) {
-                result.Error = "Base58 address starts with F or 3!";
+                result.Error = "Groestlcoin Base58 address starts with 1 or 3!";
                 return result;
             }
-            //todo: Revisit specific groestl512 validation
-            result.IsVerified = true;
+            try {
+                Groestlcoin.GroestlEncoder.Instance.DecodeData(grsAddress);
+                result.IsVerified = true;
+            }
+            catch (Exception ex) {
+                result.Error = ex.Message;
+            }
             return result;
-
-            
-            //// calculate the checksum
-            //SHA256 sha = new SHA256Managed();
-            //byte[] hash1 = sha.ComputeHash(bytesWithoutChecksum);
-            //byte[] hash2 = sha.ComputeHash(hash1);
-
-            //if (hash2[0] != dataAsByte[lengthWithoutChecksum] ||
-            //    hash2[1] != dataAsByte[lengthWithoutChecksum + 1] ||
-            //    hash2[2] != dataAsByte[lengthWithoutChecksum + 2] ||
-            //    hash2[3] != dataAsByte[lengthWithoutChecksum + 3]) {
-            //    result.Error = "Invalid Checksum!";
-            //}
-            //else {
-            //result.IsVerified = true;
-            //}
-            //return result;
         }
-
     }
 }
